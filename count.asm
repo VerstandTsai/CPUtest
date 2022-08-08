99712c9dd9bf6f07e25dfa1d4ec67149c8e66dcc
VerstandTsai <hackertsaigenz@gmail.com>
2022-08-08 19:20:31 +0800

    Put the circuit, assembler and programs here as a new repo

---
+++ count.asm
@@ -0,0 +1,11 @@
+out 15
+lda 15
+add 14
+sta 15
+sub 13
+jc  0
+hlt
+
+dat 15 0
+dat 14 1
+dat 13 255
---
+++ fib.asm
@@ -0,0 +1,13 @@
+out 14
+ldb 14
+lda 14
+add 15
+sta 14
+stb 15
+sub 13
+jc  0
+hlt
+
+dat 15 0
+dat 14 1
+dat 13 200
